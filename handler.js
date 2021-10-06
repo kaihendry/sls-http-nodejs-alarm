8aca6698a63b25cfc61b24d0c2a346914e79eebb
Kai Hendry <hendry@iki.fi>
2021-10-06 17:28:00 +0800

    initial

---
+++ handler.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: event.queryStringParameters?.code ? event.queryStringParameters.code : 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
