03135dc0fa423eebbfa4c81c185a557fcb6e6fcf
Kai Hendry <hendry@iki.fi>
2021-10-08 13:10:46 +0800

    Not 5xx notifying

--- handler.js
+++ handler.js
@@ -2,7 +2,9 @@
 
 module.exports.hello = async (event) => {
   return {
-    statusCode: event.queryStringParameters?.code ? event.queryStringParameters.code : 200,
+    statusCode: event.queryStringParameters?.code
+      ? event.queryStringParameters.code
+      : 200,
     body: JSON.stringify(
       {
         message: "Go Serverless v2.0! Your function executed successfully!",
